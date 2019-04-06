3b2388a4f6547913735ed13a6de57f54043eb311
Serge Kanashin <goujon@mail.ru>
2019-04-06 23:00:31 +0300

    Initial commit

---
+++ HasherC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HasherC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDir = @"D:\CRC_TEST";
+
+            string[] dirs = Directory.GetDirectories(rootDir, "*", SearchOption.AllDirectories);
+
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine($"{dir}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
