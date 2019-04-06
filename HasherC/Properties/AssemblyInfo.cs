5a20b9219532a0d457b0242d366ceff7305cdfaa
IllusiveMan <goujon@mail.ru>
2019-04-06 23:17:01 +0300

    Auto stash before merge of "master" and "origin/master"
    Follow up from stash

--- HasherC/Properties/AssemblyInfo.cs
+++ HasherC/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HasherC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SKS")]
 [assembly: AssemblyProduct("HasherC")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
