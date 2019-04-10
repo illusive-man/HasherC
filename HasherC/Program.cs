5dc1187188bb328643dd0eac165f4b1b06ae7d04
Serge Kanashin <goujon@mail.ru>
2019-04-10 21:41:50 +0300

    Added args parser

--- HasherC/Program.cs
+++ HasherC/Program.cs
@@ -1,18 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CommandLine;
 using System.IO;
 
 namespace HasherC
 {
     class Program
     {
+        public class Options
+        {
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+
+            [Option('c', "check", Required = false, HelpText = "Check files in directory.")]
+            public bool Check { get; set; }
+
+            [Option('p', "path", Required = false, HelpText = "Check files in directory.")]
+            public string Path { get; set; }
+        }
+        
         static void Main(string[] args)
         {
-            string rootDir = @"D:\CRC_TEST";
+            Int64 memoryBefore = GC.GetTotalMemory(true);
+            Int64 totalMemory = GC.GetTotalMemory(false);
 
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+
+                       }
+                       else if (o.Check)
+                       {
+                           Console.WriteLine($"Checking Files. Current Arguments: -c {o.Check}");
+                           Console.WriteLine("App is in Check mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose} -c {o.Check}");
+                       }
+                   });
+
+
+            string rootDir = @"D:\CRC_TEST";
+ 
             string[] dirs = Directory.GetDirectories(rootDir, "*", SearchOption.AllDirectories);
             string[] files = Directory.GetFiles(rootDir, "*.*", SearchOption.AllDirectories);
 
@@ -25,6 +58,7 @@ namespace HasherC
                     $" Size: { info.Length } bytes");
             }
 
+            Console.WriteLine($"{ memoryBefore} : { totalMemory }");
             Console.ReadKey();
         }
     }
--- HasherC/Properties/AssemblyInfo.cs
+++ HasherC/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HasherC")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Batch create hashes for files and folders with different algorithms")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SKS")]
 [assembly: AssemblyProduct("HasherC")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
