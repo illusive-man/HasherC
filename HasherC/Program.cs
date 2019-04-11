e4ba7cf433c65323c6e5e832d7cc7595b0949fa6
Sergey Kanashin <kanashin@fintech.ru>
2019-04-11 17:47:38 +0300

    Figuring out CL Parser

--- HasherC/Program.cs
+++ HasherC/Program.cs
@@ -14,32 +14,30 @@ namespace HasherC
             [Option('c', "check", Required = false, HelpText = "Check files in directory.")]
             public bool Check { get; set; }
 
-            [Option('p', "path", Required = false, HelpText = "Check files in directory.")]
+            [Option('p', "path", Required = true, HelpText = "Enter the path.")]
             public string Path { get; set; }
         }
         
         static void Main(string[] args)
         {
-            Int64 memoryBefore = GC.GetTotalMemory(true);
-            Int64 totalMemory = GC.GetTotalMemory(false);
-
+         
             Parser.Default.ParseArguments<Options>(args)
-                   .WithParsed<Options>(o =>
+                   .WithParsed(o =>
                    {
                        if (o.Verbose)
                        {
-                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose} -c {o.Check} -p {o.Path}");
                            Console.WriteLine("Quick Start Example! App is in Verbose mode!");
 
                        }
                        else if (o.Check)
                        {
-                           Console.WriteLine($"Checking Files. Current Arguments: -c {o.Check}");
+                           Console.WriteLine($"Checking Files. Current Arguments: -c {o.Check} -v {o.Verbose} -p {o.Path}");
                            Console.WriteLine("App is in Check mode!");
                        }
                        else
                        {
-                           Console.WriteLine($"Current Arguments: -v {o.Verbose} -c {o.Check}");
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose} -c {o.Check} -p {o.Path}");
                        }
                    });
 
@@ -52,13 +50,13 @@ namespace HasherC
             foreach (string file in files)
             {
                 FileInfo info = new FileInfo(file);
-                Console.WriteLine(
-                    $"Path: { Path.GetDirectoryName(file) }," +
-                    $" Name: { Path.GetFileName(file) }, " +
-                    $" Size: { info.Length } bytes");
+                //Console.WriteLine(
+                  //  $"Path: { Path.GetDirectoryName(file) }," +
+                  //  $" Name: { Path.GetFileName(file) }, " +
+                  //  $" Size: { info.Length } bytes");
             }
-
-            Console.WriteLine($"{ memoryBefore} : { totalMemory }");
+            int test = 6 / 2 * (1 + 2);
+            Console.WriteLine($"{ test }");
             Console.ReadKey();
         }
     }
