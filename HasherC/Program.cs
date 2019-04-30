6ead4730e41c46ee7da861fb7d576d1d6df02ddd
Sergey Kanashin <kanashin@fintech.ru>
2019-04-30 18:24:52 +0300

    Updated CommandLine, vars linking

--- HasherC/Program.cs
+++ HasherC/Program.cs
@@ -5,53 +5,51 @@ using NLog;
 
 namespace HasherC
 {
-    class Program
+    internal class Program
     {
-        
+
         public class Options
         {
-            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
-            public bool Verbose { get; set; }
+            [Option('i', "isinst", Required = false, HelpText = "Specify if distribution is/has installer.")]
+            public bool IsInst { get; set; }
 
-            [Option('c', "check", Required = false, HelpText = "Check files in directory.")]
-            public bool Check { get; set; }
+            [Option('d', "makedoc", Required = false, HelpText = "Specify if protocol documents have to be created.")]
+            public bool Makedox { get; set; }
 
-            [Option('p', "path", Required = true, HelpText = "Enter the path.")]
+            [Option('p', "path", Required = true, HelpText = "Enter the path to folder/image.")]
             public string Path { get; set; }
         }
-    
-        
-        static void Main(string[] args)
+
+
+        private static void Main(string[] args)
         {
-
-
-            Logger logger = LogManager.GetCurrentClassLogger();
-
-        Parser.Default.ParseArguments<Options>(args)
-                   .WithParsed(o =>
-                   {
-                       if (o.Verbose || o.Check)
-                       {
-                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose} -c {o.Check} -p {o.Path}");
-                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
-                       }
-                   });
-
-            
             string rootDir = @"D:\CRC_TEST";
- 
-            string[] dirs = Directory.GetDirectories(rootDir, "*", SearchOption.AllDirectories);
+            var logger = LogManager.GetCurrentClassLogger();
+            Parser.Default.ParseArguments<Options>(args)
+           .WithParsed(o =>
+           {
+               if (o.IsInst || o.Makedox || o.Path != null)
+               {
+                   Console.WriteLine($"Current Arguments: -i {o.IsInst} -d {o.Makedox} -p {o.Path}");
+                   rootDir = o.Path;
+               }
+           });
+
+
+             
             string[] files = Directory.GetFiles(rootDir, "*.*", SearchOption.AllDirectories);
+            string[] dirs = Directory.GetDirectories(rootDir, "*", SearchOption.AllDirectories);
+            
 
             foreach (string file in files)
             {
-                FileInfo info = new FileInfo(file);
-                //Console.WriteLine(
-                  //  $"Path: { Path.GetDirectoryName(file) }," +
-                  //  $" Name: { Path.GetFileName(file) }, " +
-                  //  $" Size: { info.Length } bytes");
+                var info = new FileInfo(file);
+                Console.WriteLine(
+                  $"Path: { Path.GetDirectoryName(file) }," +
+                  $" Name: { Path.GetFileName(file) }, " +
+                  $" Size: { info.Length } bytes");
             }
-            int test = ~256 + 1;
+            var test = ~256 + 1;
 
             Console.WriteLine($"{ test }");
             logger.Log(LogLevel.Warn, "Sample informational message, called from other context");
