808d1b6fb31141e95b675a85124da235c11c2346
Serge Kanashin <goujon@mail.ru>
2019-05-01 19:58:03 +0300

    Updated Nugets

---
+++ HasherC/Options.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace HasherC
+{
+    public class Options
+    {
+        [Option('i', "isinst", Required = false, HelpText = "Specify if distribution is/has installer.")]
+        public bool IsInst { get; set; }
+
+        [Option('d', "makedoc", Required = false, HelpText = "Specify if protocol documents have to be created.")]
+        public bool Makedox { get; set; }
+
+        [Option('p', "path", Required = true, HelpText = "Enter the path to folder/image.")]
+        public string Path { get; set; }
+    }
+}
